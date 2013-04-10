dc3678482834c2627e817cc1cabfd8fec9aa7708
Andy M <andy.dude@gmail.com>
2013-04-10 18:21:29 -0500

    Updated the JS and some new content.

--- web/scripts/datasource.js
+++ web/scripts/datasource.js
@@ -22,26 +22,40 @@ function dataSource(){
                 returnValue.links = [{'label':'Linked-In', 'link': 'http://www.linkedin.com/pub/andy-meyers/2/161/890'},
                     {'label': 'Blog', 'link':'http://andyjmeyers.blogspot.com/'},
                     {'label': 'GitHub', 'link':'https://github.com/thatguyandy27'},
-                    {'label': 'JSFiddle', 'link':'http://jsfiddle.net/user/andyjmeyers/fiddles/'}
+                    {'label': 'JSFiddle', 'link':'http://jsfiddle.net/user/andyjmeyers/fiddles/'},
+                    {'label': 'StackExchange', 'link': 'http://stackexchange.com/users/200646/andy-meyers'}
                 ];
-            break;
+	            break;
             case 'leadership':
+            	returnValue.peopleManagement = ['Manages Direct Reports', 'Performs weekly one on ones',
+            		'Helps defines goals for direct reports', 'Performs bi-yearly reviews',
+            		'Approves time/PTO/etc.', 'Provides feedback for employees', 'Leads changes in company'];
+            	returnValue.softwareManagement =['Onsite discovery and requirement gathering', 
+            		'Architects and designs implementations', 
+            		'Set up project, automated builds and other tasks.',
+            		'Creates and maintains developer items',
+            		'Manages engineering teams', 'Leads project stand-ups', 
+            		'Maintains project technical documentition',
+            		'Manages deploys and post-support requests'  ];
                 break;
             case 'books':
-                returnValue.books = [{'title': "First, Break All the Rules: What the world's Greatest Managers Do Differently"},
+                returnValue.books = [
+                	{'title': "Code Complete"},
+                	{'title': "Effective Programming: More Than Writing Code (Yes I know it is essentially the blog, but it was cheap and easy to read on the train.)"},
+                	{'title': "Head First Design Patterns"},
+                	{'title': "First, Break All the Rules: What the world's Greatest Managers Do Differently"},
                     {'title': "Drive: The Surprising Truth About What Motivates Us"},
                     {'title': "The Trusted Advisor"},
-                    {'title': "How to Win Friends and Influence People"},
-                    {'title': "Head First Design Patterns"}];
+                    {'title': "How to Win Friends and Influence People"}];
                 break;
             
             case 'engineering':
-                returnValue.langauges = ['c#', 'VB', 'JavaScript', 'Python', 'HTML 5', 'SQL'];
+                returnValue.langauges = ['c#', 'VB', 'JavaScript', 'Python',  'SQL', 'Java', 'C++'];
                 returnValue.applications = ['N-Tier Web Sites', 'Console Applications', 'Migration Applications', '3rd Party Intgrations',
                     'Web-API only applications', 'Desktop Applications', 'Mobile Applications'];
                 returnValue.tools = ['Visual Studio', 'Eclipse', 'GIT', 'SVN', 'Fiddler', 'Web debugging'];
                 returnValue.netFrameworks = ['Windows-8 (Metro) Apps', 'MVC', 'WPF', 'XAML', 'WCF', 'SQL Management Studio'];
-                returnValue.javascriptFrameworks = ['jQuery', 'Knockout', 'Backbone', 'Ember', 'mustache', 'underscore'];
+                returnValue.javascriptFrameworks = ['HTML5', 'jQuery', 'Knockout', 'Backbone', 'Ember', 'mustache', 'underscore'];
                 returnValue.softwareEngineering = [ 'Agile', 'SCRUM', 'Full-Stack development', 'Hands-On Requirements Gathering',
                     'Client Facing', 'Project Leadership'];
               //  returnValue.other = ['Strong knowledege web debugging tools', 'Fiddler',
